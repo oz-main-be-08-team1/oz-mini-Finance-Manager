1dae2cca67c8667ab3cf9d1cfbb3680f4fe3dfa4
hak1319 <gkr054@naver.com>
2025-03-04 14:19:34 +0900

    Initial Commit

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -7,4 +7,3 @@ ALLOWED_HOSTS = []
 print("*" * 100)
 print("prod")
 print("*" * 100)
-
--- config/urls/local_url.py
+++ config/urls/local_url.py
@@ -14,10 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
-from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
 schema_view = get_schema_view(
@@ -31,7 +32,11 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path("admin/", admin.site.urls),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
