04735acc659f3236efcbae54c6143fdba0632f95
hak1319 <gkr054@naver.com>
2025-03-04 18:10:58 +0900

    Create Transaction, Accounts Model

--- apps/accounts/tests.py
+++ apps/accounts/tests.py
@@ -1,3 +1,24 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
+from apps.accounts.models import Account
+
 # Create your tests here.
+User = get_user_model()
+
+
+class Test_Account_Model(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(email="test@test.com", password="test")
+        self.account = {
+            "user": self.user,
+            "account_number": "123456789",
+            "bank_code": "001",
+            "account_type": "LOAN",
+            "balance": 100,
+        }
+
+    def test_account_create(self):
+        account = Account.objects.create(**self.account)
+        self.assertEqual(account.account_number, "123456789")
+        self.assertEqual(Account.objects.count(), 1)
--- apps/user/tests.py
+++ apps/user/tests.py
@@ -1,3 +1,18 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 # Create your tests here.
+User = get_user_model()
+
+
+class Test_User_Model(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(email="test@test.com", password="test1234")
+        self.admin_user = User.objects.create_superuser(
+            email="super@super.com", password="test1234"
+        )
+
+    def test_create_user(self):
+        self.assertEqual(self.admin_user.is_admin, True)
+        self.assertEqual(self.user.is_admin, False)
+        self.assertEqual(User.objects.count(), 2)
